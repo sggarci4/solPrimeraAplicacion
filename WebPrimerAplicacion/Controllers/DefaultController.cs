cfaea703103090f1e94b1f6a593417a53a20fda5
sggarci4 <159163227+sggarci4@users.noreply.github.com>
2024-06-21 06:19:02 -0600

    Add files via upload

---
+++ WebPrimerAplicacion/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPrimerAplicacion.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Inicio()
+        {
+            return View("Inicio");
+        }
+
+        public ActionResult IrVista2()
+        {
+            return View("SegundaVista");
+        }
+    }
+}
